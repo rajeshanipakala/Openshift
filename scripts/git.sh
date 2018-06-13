3a366ea7ffef1f0e4881fa30bc7a6bc9934dcbb9
rajeshanipakala <arajeshbabu5@gmail.com>
2018-06-13 23:17:01 +0000

    Test commit

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-rajeshanipakala
-5Arajesh@
+
 GIT=`which git`
 REPO_DIR=/home/svc-vxby-ose/Openshift/
 cd ${REPO_DIR}
 ${GIT} add --all .
 ${GIT} commit -m "Test commit"
 ${GIT} push https://github.com/rajeshanipakala/Openshift.git master
+$rajeshanipakala
+$5Arajesh@
